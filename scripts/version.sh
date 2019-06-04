16d84624d9ace56a0e0b0f2cb7b9b9ead4c00bca
Yehiyam Livneh <yehiyam@gmail.com>
2019-06-04 16:28:29 +0300

    fix ci

--- scripts/version.sh
+++ scripts/version.sh
@@ -2,7 +2,7 @@
 set -ev
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-if ([ "$TRAVIS_BRANCH" == "master" ] || [ ! -z "$TRAVIS_TAG" ]) && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ ! -z "$VERSION" ]; then
+if ([ "$TRAVIS_BRANCH" == "master" ] || [ ! -z "$TRAVIS_TAG" ]) && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   git config --global user.email "travis@hkube.io"
   git config --global user.name "Travis CI"
   git clone --single-branch --branch ${VERSION} --depth 1 https://github.com/kube-HPC/hkube.git ~/hkube
