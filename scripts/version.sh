eafe1ad46af68043aa47cebd35c34eaab16cccc6
Yehiyam Livneh <yehiyam@gmail.com>
2019-06-04 12:56:45 +0300

    fix CI

--- scripts/version.sh
+++ scripts/version.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 set -ev
 if ([ "$TRAVIS_BRANCH" == "master" ] || [ ! -z "$TRAVIS_TAG" ]) && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ ! -z "$VERSION" ]; then
-    git config --global user.email "travis@hkube.io"
-    git config --global user.name "Travis CI"
-    git clone --single-branch --branch ${VERSION} --depth 1 git@github.com:kube-HPC/hkube.git ~/hkube
-    cd ~/hkube
-    npm i
-  else
-    echo "version skiped!"
-  fi
+  git config --global user.email "travis@hkube.io"
+  git config --global user.name "Travis CI"
+  git clone --single-branch --branch ${VERSION} --depth 1 git@github.com:kube-HPC/hkube.git ~/hkube
+  cd ~/hkube
+  npm i
+  HKUBE_FOLDER=/home/travis/hkube ./getVersions.js
+else
+  echo "version skiped!"
+fi
