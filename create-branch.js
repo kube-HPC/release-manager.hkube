06dcae5a132dfafa351d708483283592d6cf2fad
Yehiyam Livneh <yehiyam@gmail.com>
2019-09-01 11:42:07 +0300

    brach for 1.1.1085

--- create-branch.js
+++ create-branch.js
@@ -1,14 +1,17 @@
 const fs = require('fs');
 const path = require('path');
 const syncSpawn = require('spawn-sync');
-const baseFolder = path.resolve(path.join(process.env.HOME,'dev','hkube'));
-const main = async()=>{
+const baseFolder = path.resolve(path.join(process.env.HOME, 'dev', 'hkube'));
+const main = async () => {
     const versions = JSON.parse(fs.readFileSync('./version.json'));
-    const branchName='release_beta_v1_0'
-    for(let v of versions.versions){
-        try{
-            console.dir(v);
-            const repoFolder =path.join(baseFolder,v.project);
+    const branchName = 'release_v1_1_1085'
+    for (let v of versions.versions) {
+        try {
+            console.log(`${v.project}: ${v.tag}`);
+            const repoFolder = path.join(baseFolder, v.project);
+            // await syncSpawn('grep', ['-Po', '\\"version\\": \\"\\K(.*)(?=\\",)', 'package.json'], { cwd: repoFolder ,stdio: 'inherit'})
+            // await syncSpawn('git',['status'],{cwd: repoFolder,stdio: 'inherit' })
+
             // await syncSpawn('git',['checkout',`${v.tag}`],{cwd: repoFolder,stdio: 'inherit' })
             // await syncSpawn('git',['checkout','-b',branchName],{cwd: repoFolder,stdio: 'inherit' })
             // await syncSpawn('git',['push','--set-upstream','origin',branchName],{cwd: repoFolder,stdio: 'inherit' })
@@ -16,12 +19,12 @@ const main = async()=>{
 
             await syncSpawn('npm',['version','minor'],{cwd: repoFolder,stdio: 'inherit' })
             await syncSpawn('git',['push','--follow-tags'],{cwd: repoFolder,stdio: 'inherit' })
-            
+
             await syncSpawn('git',[`status`],{cwd: repoFolder,stdio: 'inherit' })
-            
-    
+
+
         }
-        catch(e){
+        catch (e) {
             console.error(e)
         }
     }
