f101f367ebbeb3dc8730e1f835c409c8eee0b58f
Yehiyam Livneh <yehiyam@gmail.com>
2020-11-30 13:20:47 +0200

    ci - github actions

--- scripts/version.sh
+++ scripts/version.sh
@@ -1,22 +1,16 @@
 #!/bin/bash
-set -ev
+set -evo pipefail
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-if ([ "$TRAVIS_BRANCH" == "master" ] || [ ! -z "$TRAVIS_TAG" ]) && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-  git config --global user.email "travis@hkube.io"
-  git config --global user.name "Travis CI"
-  export BRANCH=${VERSION:-master}
-  echo cloning ${BRANCH} branch
-  git clone --single-branch --branch ${BRANCH} https://github.com/kube-HPC/hkube.git ~/hkube
-  cd ~/hkube
-  npm i
-  if [ -z $VERSION ]
-  then
-    echo No Version. Defaulting to latest
-    export VERSION=$(git describe --abbrev=0 --tags)  
-  fi
-  echo using version ${VERSION}
-  HKUBE_FOLDER=/home/travis/hkube ${DIR}/getVersions.js
-else
-  echo "version skiped!"
+export BRANCH=${VERSION:-master}
+echo cloning ${BRANCH} branch
+git clone --single-branch --branch ${BRANCH} https://github.com/kube-HPC/hkube.git /tmp/hkube
+cd /tmp/hkube
+npm i
+if [ -z $VERSION ]
+then
+  echo No Version. Defaulting to latest
+  export VERSION=$(git describe --abbrev=0 --tags)  
 fi
+echo using version ${VERSION}
+HKUBE_FOLDER=/tmp/hkube ${DIR}/getVersions.js
